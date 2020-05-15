000c7a855834037aae15f67246c01f824aa7bd79
Arpan <arpanchowdhury050@gmail.com>
2020-05-15 19:33:01 +0530

    Updates

---
+++ precache-manifest.7679bd3dc0dc330755f4c4223799d130.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153a8c2d205a74541136526e566f0e3d",
+    "url": "/React-TODO/index.html"
+  },
+  {
+    "revision": "033641320ae3c483f126",
+    "url": "/React-TODO/static/css/main.22f1ff1e.chunk.css"
+  },
+  {
+    "revision": "16c24a359940966862ad",
+    "url": "/React-TODO/static/js/2.599cf32c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-TODO/static/js/2.599cf32c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "033641320ae3c483f126",
+    "url": "/React-TODO/static/js/main.195bcdf1.chunk.js"
+  },
+  {
+    "revision": "bb9049d10370a4780c61",
+    "url": "/React-TODO/static/js/runtime-main.742888f5.js"
+  }
+]);
